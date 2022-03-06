4a011420c808fac06cd0fe32f1f7940bea980ef2
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-03-06 08:48:53 -0300

    style: update styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
-import "../../styles/globals.css";
 import { AuthProvider } from "../contexts/AuthContext";
+
+import "../../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
