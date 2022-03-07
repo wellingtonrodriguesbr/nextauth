11f1d0fec2645170f142fcbd1626c065e3d2c7eb
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-03-07 06:59:31 -0300

    chore: in case of error, log out user

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosError } from "axios";
 import { parseCookies, setCookie } from "nookies";
+import { signOut } from "../contexts/AuthContext";
 
 let cookies = parseCookies();
 let isRefreshing = false;
@@ -73,8 +74,9 @@ api.interceptors.response.use(
           });
         });
       } else {
-        //deslogar usúario
+        signOut();
       }
     }
+    return Promise.reject(error);
   }
 );
